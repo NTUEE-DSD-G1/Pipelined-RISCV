054af0e5eaf4d53737c911ad2dc7cbe3df7635f8
Jim-CTChen <b07901036@ntu.edu.tw>
2021-06-03 13:34:04 +0800

    upload jim's ICache

--- Baseline/src/Icache.v
+++ Baseline/src/Icache.v
@@ -14,252 +14,142 @@ module Icache(
     mem_wdata,
     mem_ready
 );
-
-
+    
 //==== input/output definition ============================
-    input               clk;
+    input          clk;
     // processor interface
-    input               proc_reset;
-    input               proc_read, proc_write;
-    input       [29:0]  proc_addr;      // proc_addr[1:0] is the offset
-                                        // proc_addr[29:2] is the mem_addr when accessing the memory
-                                        // Use proc_addr[3:2] as the set number of the cache
-                                        // Use proc_addr[29:4] as the tag of the cache
-    input       [31:0]  proc_wdata;
-    output reg          proc_stall;
-    output reg  [31:0]  proc_rdata;
-    
+    input          proc_reset;
+    input          proc_read, proc_write;
+    input   [29:0] proc_addr;
+    input   [31:0] proc_wdata;
+    output         proc_stall;
+    output  [31:0] proc_rdata;
     // memory interface
-    input       [127:0] mem_rdata;
-    input               mem_ready;
-    output reg          mem_read, mem_write;
-    output reg [27:0]   mem_addr;
-    output reg [127:0]  mem_wdata;
+    input  [127:0] mem_rdata;
+    input          mem_ready;
+    output         mem_read, mem_write;
+    output  [27:0] mem_addr;
+    output [127:0] mem_wdata;
 
-    // state parameters
-    parameter REQUEST       = 3'b000;
-    parameter READMEM       = 3'b001;
-    parameter PRELOAD       = 3'b010;
-    
-    //==== wire/reg definition ================================
-    integer i;
-    
-    // the storage of the cache (8 blocks, each with 4 words (128-bits))
-    reg  [127:0] cache_data         [0:7]; 
-    reg  [127:0] next_cache_data    [0:7];  
-    reg  [24:0]  cache_tag          [0:7];
-    reg  [24:0]  next_cache_tag     [0:7];
-    reg          cache_valid        [0:7];
-    reg          next_cache_valid   [0:7];
+//==== parameter ==========================================
+parameter NUM_OF_SET = 4;
+parameter NUM_OF_WAY = 2;
 
-    // state
-    reg  [2:0]   state, next_state;
-    
-    wire [1:0]  set_num;
-    reg  [2:0]  index1, index2;
-    wire        hit1, hit2;
-    wire [25:0] tag;
-    wire [1:0]  offset;
-    wire        hit;
-    reg  [31:0] target_cache_data1, target_cache_data2, target_mem_data; 
+parameter IDLE = 1'b0;
+parameter READ_MEM = 1'b1;
 
-    wire        ReadHit, ReadMiss;
+//==== wire/reg definition ================================
+// outputs 
+reg         proc_stall;
+reg [ 31:0] proc_rdata;
+reg         mem_read, mem_write;
+reg [ 27:0] mem_addr;
+reg [127:0] mem_wdata;
 
-    //==== additional wire/reg definition ================================
-    wire [25:0] preload_tag;
-    wire [1:0]  preload_set_num;
-    wire [27:0] preload_addr;
-    wire [2:0]  preload_index;
+// FFs
+reg         state, next_state;
 
-    //==== combinational circuit ==============================
-    assign set_num = proc_addr[3:2];
-    assign tag = proc_addr[29:4];
-    assign offset = proc_addr[1:0];
-    assign hit1 = ((proc_addr[29:4] == cache_tag[index1]) && cache_valid[index1]);
-    assign hit2 = ((proc_addr[29:4] == cache_tag[index2]) && cache_valid[index2]);
-    assign hit = hit1 || hit2;
-    
-    assign ReadHit = proc_read && hit;
-    assign ReadMiss = proc_read && !hit;
-
-    //=========================================================
-    assign preload_tag = (set_num == 2'b11) ? (tag + 1) : tag;
-    assign preload_set_num = set_num + 1;
-    assign preload_addr = {preload_tag, preload_set_num};
-    assign preload_index = {preload_set_num, 1'b0};
-
-    always@(*) begin
-        case(set_num)
-            0: begin
-                index1 = 3'd0;
-                index2 = 3'd1;
-            end
-            1: begin
-                index1 = 3'd2;
-                index2 = 3'd3;
-            end
-            2: begin
-                index1 = 3'd4;
-                index2 = 3'd5;
-            end
-            3: begin
-                index1 = 3'd6;
-                index2 = 3'd7;
-            end
-        endcase
-    end
-
-    always@(*) begin
-        case(offset)
-            3: begin
-                target_cache_data1 = cache_data[index1][127:96];
-                target_cache_data2 = cache_data[index2][127:96];
-                target_mem_data = mem_rdata[127:96];
-            end
-            2: begin
-                target_cache_data1 = cache_data[index1][95:64];
-                target_cache_data2 = cache_data[index2][95:64];
-                target_mem_data = mem_rdata[95:64];
-            end
-            1: begin
-                target_cache_data1 = cache_data[index1][63:32];
-                target_cache_data2 = cache_data[index2][63:32];
-                target_mem_data = mem_rdata[63:32];
-            end
-            0: begin
-                target_cache_data1 = cache_data[index1][31:0];
-                target_cache_data2 = cache_data[index2][31:0];
-                target_mem_data = mem_rdata[31:0];
-            end
-            default: begin
-                target_cache_data1 = 0;
-                target_cache_data2 = 0;
-                target_mem_data = 0;
-            end
-        endcase
-    end
-    
-    // output, variable logic
-    always@(*) begin
-        // default value (FF) 
-        for (i = 0; i < 8; i = i+1) begin
-            next_cache_data[i] = cache_data[i];
-            next_cache_tag[i] = cache_tag[i];
-            next_cache_valid[i] = cache_valid[i];
-        end
-        
-        // default value (output)
-        proc_stall = 0;
-        proc_rdata = 0;
-        mem_read = 0;
-        mem_write = 0;
-        mem_addr = 0;
-        mem_wdata = 0;
-
-        case(state)
-            REQUEST: begin
-                // read hit -> directly return the data
-                if(ReadHit) begin
-                    // ========================================================================
-                    // Prefetch_strategy: Set memory in read mode when sequentially HIT appears
-                    // ========================================================================
-                    mem_read = 1;
-                    mem_addr = preload_addr;
-                    proc_rdata = hit1 ? target_cache_data1 : target_cache_data2;
-                end
-                
-                // read_miss -> stall and set the read mode of memory
-                if(ReadMiss) begin
-                    proc_stall = 1;
-                    mem_addr = proc_addr[29:2];
-                    mem_read = 1;
-                end
-            end
-
-            READMEM: begin
-                // default: set the read mode of memory
-                mem_addr = proc_addr[29:2];
-                mem_read = 1;
-                proc_stall = 1;
-                // when mem ready (come from readmiss), cancel the read mode, update the cache data, tag 
-                // and return the target data (read from memory)
-                if(mem_ready) begin
-                    
-                    // ========================================================================
-                    // Prefetch_strategy: Set memory in read mode when sequentially HIT appears
-                    // ========================================================================
-                    mem_read = 1;
-                    mem_addr = preload_addr;
-                    
-                    // replace cache strategy
-                    // First move the data to index2 in a set
-                    next_cache_valid[index2] = cache_valid[index1];
-                    next_cache_tag[index2] = cache_tag[index1];
-                    next_cache_data[index2] = cache_data[index1];
-                    
-                    // Then write the new data to the index1
-                    next_cache_valid[index1] = 1;
-                    next_cache_tag[index1] = tag;
-                    
-                    if(ReadMiss) begin
-                        proc_stall = 0;
-                        proc_rdata = target_mem_data;
-                        next_cache_data[index1] = mem_rdata;
-                    end
-                end
-            end
-            
-        endcase
-    end
-
-    // next state logic
-    always@(*) begin
-        next_state = state;
-        case(state)
-            REQUEST: begin
-                // read hit -> remain REQUEST
-                if(ReadHit) begin
-                    next_state = REQUEST;
-                end
-                
-                // read_miss -> go to READMEM
-                if(ReadMiss) begin
-                    next_state = READMEM;
-                end
-            end
-            
-            READMEM: begin
-                // when mem ready (come from readmiss), go back to REQUEST
-                if(mem_ready) begin
-                    next_state = REQUEST;
-                end
-            end
-
-            default: begin
-                next_state = state;
-            end
-        endcase
-    end
+reg [127:0] data[0:NUM_OF_SET-1][0:NUM_OF_WAY-1],  next_data[0:NUM_OF_SET-1][0:NUM_OF_WAY-1];
+reg [ 25:0] tag[0:NUM_OF_SET-1][0:NUM_OF_WAY-1],   next_tag[0:NUM_OF_SET-1][0:NUM_OF_WAY-1];
+reg         valid[0:NUM_OF_SET-1][0:NUM_OF_WAY-1], next_valid[0:NUM_OF_SET-1][0:NUM_OF_WAY-1];
+reg         old[0:NUM_OF_SET-1], next_old[0:NUM_OF_SET-1];
 
 
-    //==== sequential circuit =================================
-    always@( posedge clk ) begin
-        if( proc_reset ) begin
-            for (i = 0; i < 8; i = i+1) begin
-                cache_data[i] <= 0;
-                cache_tag[i] <= 0;
-                cache_valid[i] <= 0;
-            end
-            state <= 0; // request
-        end
-        else begin
-            for (i = 0; i < 8; i = i+1) begin
-                cache_data[i] <= next_cache_data[i];
-                cache_tag[i] <= next_cache_tag[i];
-                cache_valid[i] <= next_cache_valid[i];
-            end
-            state <= next_state;
+wire read;
+wire [25:0] in_tag;
+wire [1:0] set_idx;
+wire [1:0] word_idx;
+
+//==== combinational circuit ==============================
+integer i, l;
+assign read = proc_read;
+assign in_tag = proc_addr[29:4];
+assign set_idx = proc_addr[3:2];
+assign word_idx = proc_addr[1:0];
+
+always @(*) begin
+    next_state = state;
+    proc_stall = 1'b0;
+    proc_rdata = 0;
+    mem_read = 1'b0;
+    mem_write = 1'b0;
+    mem_addr = 0;
+    mem_wdata = 127'b0;
+    for (i = 0; i < NUM_OF_SET; i=i+1) begin
+        next_old[i] = old[i];
+        for (l = 0; l < NUM_OF_WAY; l=l+1) begin
+            next_data[i][l] = data[i][l];
+            next_tag[i][l] = tag[i][l];
+            next_valid[i][l] = valid[i][l];
         end
     end
+    case (state)
+        IDLE: begin
+            if (read) begin
+                if (valid[set_idx][0] && (tag[set_idx][0] == in_tag)) begin // hit
+                    next_state = IDLE;
+                    proc_rdata = data[set_idx][0][(word_idx+1)*32-1 -: 32];
+                    next_old[set_idx] = 1'b1;
+                end
+                else if (valid[set_idx][1] && (tag[set_idx][1] == in_tag)) begin
+                    next_state = IDLE;
+                    proc_rdata = data[set_idx][1][(word_idx+1)*32-1 -: 32];
+                    next_old[set_idx] = 1'b0;
+                end
+                else begin
+                    next_state = READ_MEM;
+                    mem_read = 1'b1;
+                    mem_addr = { in_tag, set_idx };
+                    proc_stall = 1'b1;
+                end
+            end
+        end
+        READ_MEM: begin
+            if (mem_ready) begin
+                next_state = IDLE;
+                mem_read = 1'b0;
+                proc_stall = 1'b0;
+                next_old[set_idx] = ~old[set_idx];
+                next_valid[set_idx][old[set_idx]] = 1'b1;
+                next_tag[set_idx][old[set_idx]] = in_tag;
+                next_data[set_idx][old[set_idx]] = mem_rdata;
+                proc_rdata = mem_rdata[(word_idx+1)*32-1 -: 32];
+            end
+            else begin
+                next_state = READ_MEM;
+                mem_read = 1'b1;
+                mem_addr = { in_tag, set_idx };
+                proc_stall = 1'b1;
+            end
+        end
+    endcase
+end
+
+//==== sequential circuit =================================
+integer j, k;
+always@( posedge clk ) begin
+    if( proc_reset ) begin
+        state <= IDLE;
+        for (j = 0; j < NUM_OF_SET; j=j+1) begin
+            old[j] = 0;
+            for (k = 0; k < NUM_OF_WAY; k=k+1) begin
+                data[j][k] = 128'b0;
+                tag[j][k] = 0;
+                valid[j][k] = 0;
+            end
+        end
+    end
+    else begin
+        state <= next_state;
+        for (j = 0; j < NUM_OF_SET; j=j+1) begin
+            old[j] = next_old[j];
+            for (k = 0; k < NUM_OF_WAY; k=k+1) begin
+                data[j][k] = next_data[j][k];
+                tag[j][k] = next_tag[j][k];
+                valid[j][k] = next_valid[j][k];
+            end
+        end
+    end
+end
 
 endmodule
